38698a328c76697cb62983eae8a8215cb9183de3
Kondziowy <konrad.brodzik@gmail.com>
2021-01-04 22:26:44 +0100

    Added randomizer and refactored ifs to match

--- src/main.rs
+++ src/main.rs
@@ -15,25 +15,39 @@ Hello there, I'm a friendly llama!";
 
 const HELP_COMMAND: &str = "%help";
 const SHOW_COMMAND: &str = "%pokazlame";
+const YESNO_COMMAND: &str = "lame czy";
+
+const YESNO_RESPONSES: [&str; 10] = ["Tak.", "Nie.", "Oczywiście!", "Zapomnij!", "да!", "Нет!", "Jeszcze jak!", "No chyba nie.", "Ja!", "Nein!"];
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == HELP_COMMAND {
-            println!("Got help");
-            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
-                println!("Error sending message: {:?}", why);
+        println!("Got {}", msg.content);
+        let mut response = String::new();
+        match msg.content.as_str() {
+            HELP_COMMAND => {
+                response = String::from(HELP_MESSAGE);
+            }
+            SHOW_COMMAND => {
+                response = String::from(findimage().await.unwrap());
+            }
+            &_ => {
+                if msg.content.starts_with(YESNO_COMMAND) {
+                    let reply = format!(":8ball: {}", YESNO_RESPONSES[rand::thread_rng().gen_range(0..10)]);
+                    if let Err(why) = msg.reply(&ctx.http, reply).await {
+                        println!("Error sending message: {:?}", why);
+                    }
+                }
             }
         }
-        if msg.content == SHOW_COMMAND {
-            println!("Got pokazlame");
-            let image = findimage().await.unwrap();
-            println!("{}", image);
-            if let Err(why) = msg.channel_id.say(&ctx.http, image).await {
-                println!("Error sending message: {:?}", why);
-            }
+        if response.len() == 0 {
+            return;
+        }
+        println!("Sending response {}", response);
+        if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
+            println!("Error sending message: {:?}", why);
         }
     }
 
@@ -43,8 +57,7 @@ impl EventHandler for Handler {
 }
 
 async fn findimage() -> Result<String, Box<dyn std::error::Error>> {
-    let res =
-        reqwest::get("https://www.google.com/search?q=llama&sclient=img&tbm=isch").await;
+    let res = reqwest::get("https://www.google.com/search?q=llama&sclient=img&tbm=isch").await;
     let random_num = rand::thread_rng().gen_range(0..10);
     let body = res.unwrap().text().await.unwrap();
 
